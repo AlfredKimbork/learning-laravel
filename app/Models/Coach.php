32d265834bc14ec8ad4c35fd1b4f071d332afc5f
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-06-18 13:59:09 +0200

    Trying to use Eloquent

---
+++ app/Models/Coach.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coach extends Model
+{
+    protected $fillable = ['name', 'experience', 'bio'];
+
+    /** @use HasFactory<\Database\Factories\CoachFactory> */
+    use HasFactory;
+}
