a9efd914a55e660cd19f9af8ccd4fd0b15fd6b95
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-06-18 11:47:16 +0200

    styling + minor fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home | Network</title>
+
+  @vite('resources/css/app.css')
 </head>
-<body>
+<body class="text-center px-8 py-12">
   <h1>Welcome to the Network</h1>
-  <p>Click the button bellow to view the list of users</p>
-  <a href="/users" class="btn">Find Users!</a>
+  <p>Click the button bellow to view the list of coaches</p>
+  <a href="/coaches" class="btn mt-4 inline-block">Find Users!</a>
 </body>
 </html>
