0baa4c692e379615740f0f6e64ae6a518b713e92
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-06-18 10:10:13 +0200

    getting to know how routes work

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Users | Network</title>
+</head>
+<body>
+  <h2>currently available users</h2>
+  <ul>
+    <li>users here</li>
+  </ul>
+  
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Network</title>
+  <title>Home | Network</title>
 </head>
 <body>
   <h1>Welcome to the Network</h1>
   <p>Click the button bellow to view the list of users</p>
-  <a href="" class="btn">Find Users!</a>
+  <a href="/users" class="btn">Find Users!</a>
 </body>
 </html>
